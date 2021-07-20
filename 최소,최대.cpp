3499fba73cd08aca3b8ce4c8a65cdc0c327a9dff
Chan531 <chan657312@gmail.com>
2021-07-20 15:19:35 +0900

    백준 10818번 최소, 최대

---
+++ 최소,최대.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	int n, num;
+	vector<int> v;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	cout << v[0] << ' ' << v[n - 1];
+	return 0;
+}
