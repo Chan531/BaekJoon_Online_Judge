7b0c41026b3a216c7ac44a013d5f19ee740e9ebd
Chan531 <chan657312@gmail.com>
2021-08-05 17:07:11 +0900

    백준 1308번 D-day

---
+++ 네번째점.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+int main(void)
+{
+	int x, y;
+	map<int, int> xx;
+	map<int, int> yy;
+	map<int, int>::iterator it;
+	for (int i = 0; i < 3; i++)
+	{
+		cin >> x >> y;
+		xx[x]++;
+		yy[y]++;
+	}
+	for (it = xx.begin(); it != xx.end(); it++)
+	{
+		if (it->second != 2)
+			cout << it->first << ' ';
+	}
+	for (it = yy.begin(); it != yy.end(); it++)
+	{
+		if (it->second != 2)
+			cout << it->first;
+	}
+	return 0;
+}
