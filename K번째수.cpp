2c01b1d165d0bc4396c63961c630dad5856f394e
Chan531 <chan657312@gmail.com>
2021-08-08 18:01:39 +0900

    백준 11004번 K 번째 수

---
+++ K번째수.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, num, k;
+	cin >> n >> k;
+	vector<int> v;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end());
+	cout << v[k - 1];
+	return 0;
+}
