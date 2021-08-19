bb08695b3e79ea2f75056184e9f20bc025487111
Chan531 <chan657312@gmail.com>
2021-08-19 16:27:06 +0900

    백준 5565번 영수증

---
+++ 영수증.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n, num;
+	for (int i = 0; i < 10; i++)
+	{
+		if (i == 0)
+		{
+			cin >> n;
+		}
+		else
+		{
+			cin >> num;
+			n -= num;
+		}
+	}
+	cout << n;
+	return 0;
+}
