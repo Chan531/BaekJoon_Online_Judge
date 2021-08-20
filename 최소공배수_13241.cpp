6ffb4a6434c3436c2ff1657584fdcb0a54c4ffe8
Chan531 <chan657312@gmail.com>
2021-08-20 14:08:06 +0900

    백준 최소공배수 13241번

---
+++ 최소공배수_13241.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int n, int m)
+{
+	if (m == 0)
+		return n;
+	else
+		return gcd(m, n % m);
+}
+
+int main(void)
+{
+	long long int n, m;
+	cin >> n >> m;
+	cout << n * m / gcd(n, m);
+	return 0;
+}
