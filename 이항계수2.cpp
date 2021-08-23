49d0a42040174b80c2b449abe8f6af76f36b9640
Chan531 <chan657312@gmail.com>
2021-08-23 14:17:03 +0900

    백준 11051번 이항 계수 2 (언젠가 꼭 모듈러 연산 & 페르마의 소정리로 풀어보자

---
+++ 이항계수2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+	int n, k;
+	cin >> n >> k;
+	vector<int> v(501502);
+	v[0] = 1;
+	v[1] = 1;
+	v[2] = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		int start = i * (i + 1) / 2;
+		int end = (i + 2) * (i + 1) / 2;
+		for (int j = start; j < end; j++)
+		{
+			if (j == start || j == end - 1)
+				v[j] = 1;
+			else
+				v[j] = (v[j - i - 1] + v[j - i]) % 10007;
+		}
+	}
+	cout << v[n * (n + 1) / 2 + k];
+	return 0;
+}
