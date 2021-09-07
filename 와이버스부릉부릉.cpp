6725c1832c76a6cc292f70c911060d2cdd017ee5
Chan531 <chan657312@gmail.com>
2021-09-08 00:43:28 +0900

    백준 14645번 와이버스 부릉부릉

---
+++ 와이버스부릉부릉.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n, k, a, b;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b;
+	}
+	cout << "ºñ¿ÍÀÌ";
+	return 0;
+}
