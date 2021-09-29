7bfdbed1fc228a9cc897b8aabd053358d10ee2e7
Chan531 <chan657312@gmail.com>
2021-09-29 10:40:22 +0900

    백준 14656번 조교는 새디스트야!!

---
+++ 조교는새디스트야!!.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n, num, count = 0;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> num;
+		if (num != i)
+			count++;
+	}
+	cout << count;
+	return 0;
+}
