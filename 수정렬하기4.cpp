1bec48cffa0c96ead857447b5940593ba5c1d022
Chan531 <chan657312@gmail.com>
2021-11-12 15:03:40 +0900

    백준 11931번 수 정렬하기 4

---
+++ 수정렬하기4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, num;
+	vector<int> v;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(v.begin(), v.end(), greater<int>());
+	for (auto i : v)
+		cout << i << '\n';
+	return 0;
+}
