a46d5012b88d3a156706b573dd904e56b56ced87
Chan531 <chan657312@gmail.com>
2022-02-24 11:05:37 +0900

    백준 17070번 파이프 옮기기 1

---
+++ 파이프옮기기1.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, map[17][17], dp[17][17][3], diag[3][2] = { {0, 1},{1, 0},{1,1} };
+
+void input()
+{
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			cin >> map[i][j];
+}
+
+int dfs(int y, int x, int status)
+{
+	if (y == n && x == n)
+		return 1;
+
+	if (dp[y][x][status])
+		return dp[y][x][status];
+
+	if (status == 1)
+	{
+		if (y >= 0 && y <= n && x >= 0 && x < n && !map[y][x + 1])
+			dp[y][x][status] += dfs(y, x + 1, status);
+
+		for (int i = 0; i < 3; i++)
+		{
+			int ny = y + diag[i][0];
+			int nx = x + diag[i][1];
+
+			if (ny < 0 || ny > n || nx < 0 || nx > n || map[ny][nx])
+				break;
+
+			if (i == 2)
+				dp[y][x][status] += dfs(ny, nx, 3);
+		}
+	}
+
+	if (status == 2)
+	{
+		if (y >= 0 && y < n && x >= 0 && x <= n && !map[y + 1][x])
+			dp[y][x][status] += dfs(y + 1, x, status);
+
+		for (int i = 0; i < 3; i++)
+		{
+			int ny = y + diag[i][0];
+			int nx = x + diag[i][1];
+
+			if (ny < 0 || ny > n || nx < 0 || nx > n || map[ny][nx])
+				break;
+
+			if (i == 2)
+				dp[y][x][status] += dfs(ny, nx, 3);
+		}
+	}
+
+	if (status == 3)
+	{
+		if (y >= 0 && y <= n && x >= 0 && x < n && !map[y][x + 1])
+			dp[y][x][status] += dfs(y, x + 1, 1);
+
+		if (y >= 0 && y < n && x >= 0 && x <= n && !map[y + 1][x])
+			dp[y][x][status] += dfs(y + 1, x, 2);
+
+		for (int i = 0; i < 3; i++)
+		{
+			int ny = y + diag[i][0];
+			int nx = x + diag[i][1];
+
+			if (ny < 0 || ny > n || nx < 0 || nx > n || map[ny][nx])
+				break;
+
+			if (i == 2)
+				dp[y][x][status] += dfs(ny, nx, status);
+		}
+	}
+
+	return dp[y][x][status];
+}
+
+void solution()
+{
+	cout << dfs(1, 2, 1);
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+	return 0;
+}
