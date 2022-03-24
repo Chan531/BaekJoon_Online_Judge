9b0de13cfcb3e2e3539c85da01f0ca696429bc9b
Chan531 <chan657312@gmail.com>
2022-03-24 14:46:57 +0900

    백준 24723번 녹색거탑

---
+++ 녹색거탑.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	cout << pow(2, n);
+	return 0;
+}
