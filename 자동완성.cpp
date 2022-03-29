fd737c94a2be53afd46f10595fe385b4d3eef29b
Chan531 <chan657312@gmail.com>
2022-03-29 14:10:04 +0900

    백준 24883번 자동완성

---
+++ 자동완성.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	char c;
+	cin >> c;
+	if (c == 'N' || c == 'n')
+		cout << "Naver D2";
+	else
+		cout << "Naver Whale";
+	return 0;
+}
