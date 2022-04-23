50a35c5ca755f45a3aad2cffe6c392109c4a7550
Chan531 <chan657312@gmail.com>
2022-04-23 16:28:13 +0900

    백준 4313번 Quicksum

---
+++ Quicksum.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+	string line;
+	while (getline(cin, line))
+	{
+		if (line == "#")
+			break;
+
+		int ans = 0;
+
+		for (int i = 0; i < line.length(); i++)
+		{
+			if (line[i] == ' ')
+				continue;
+
+			ans += (line[i] - '@') * (i + 1);
+		}
+
+		cout << ans << '\n';
+	}
+	return 0;
+}
