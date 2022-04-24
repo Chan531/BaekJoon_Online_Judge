c9c57fb6f925adee6f229b180cdf9bff1681e2c1
Chan531 <chan657312@gmail.com>
2022-04-24 22:17:25 +0900

    백준 10972번 다음 순열

---
+++ 다음순열.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, arr[10000];
+
+void input()
+{
+	cin >> n;
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+}
+
+void solution()
+{
+	next_permutation(arr, arr + n);
+
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] != i + 1)
+			break;
+
+		if (i == n - 1)
+		{
+			cout << -1;
+			return;
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << ' ';
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+	return 0;
+}
