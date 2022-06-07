3cfb06d7541d78c457097b8ef0b0c71b3d181fbe
Chan531 <chan657312@gmail.com>
2022-06-07 10:32:23 +0900

    백준 2295번 세 수의 합

--- 세수의합.cpp
+++ 세수의합.cpp
@@ -77,3 +77,63 @@ int main(void)
 	solve();
 	return 0;
 }
+
+/*  C++ STL binary_search
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, arr[1000], sumarr[500500], idx;
+
+void input()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+}
+
+void solution()
+{
+	sort(arr, arr + n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i; j < n; j++)
+			sumarr[idx++] = arr[i] + arr[j];
+	}
+
+	sort(sumarr, sumarr + idx);
+
+	for (int i = n - 1; i >= 0; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			int num = arr[i] - arr[j];
+
+			if (binary_search(sumarr, sumarr + idx, num))
+			{
+				cout << arr[i];
+				return;
+			}
+		}
+	}
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+	return 0;
+}
+*/
