bfb2996053385ec2847f7e069d6d2449e8cee412
Chan531 <chan657312@gmail.com>
2022-07-14 11:20:43 +0900

    2302번 극장 좌석

---
+++ 극장좌석.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m, seat[41], vip[41];
+
+void input()
+{
+	cin >> n >> m;
+
+	for (int i = 0; i < m; i++)
+		cin >> vip[i];
+}
+
+void solution()
+{
+	seat[0] = 1;
+	seat[1] = 1;
+
+	for (int i = 2; i <= n; i++)
+		seat[i] = seat[i - 1] + seat[i - 2];
+
+	int ans = 1, idx = 0;
+
+	for (int i = 0; i < m; i++)
+	{
+		ans *= seat[vip[i] - idx - 1];
+		idx = vip[i];
+	}
+
+	cout << ans * seat[n - idx];
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+	return 0;
+}
