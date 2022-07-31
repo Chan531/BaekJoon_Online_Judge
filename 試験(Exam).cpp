23a91e3b3ee45214150e2a7515e3f89226efa68d
Chan531 <chan657312@gmail.com>
2022-07-31 14:48:11 +0900

    백준 18411번 試験 (Exam)

---
+++ 試験(Exam).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[3];
+
+void input()
+{
+	for (int i = 0; i < 3; i++)
+		cin >> arr[i];
+}
+
+void solution()
+{
+	sort(arr, arr + 3);
+
+	cout << arr[1] + arr[2];
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+	return 0;
+}
