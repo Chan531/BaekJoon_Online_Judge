d1ba29adfca4a64adf6068c04f35b96fdc8ed496
Chan531 <chan657312@gmail.com>
2022-09-10 22:10:45 +0900

    백준 23810번 골뱅이 찍기 - 뒤집힌 ㅋ

---
+++ 골뱅이찍기-뒤집힌ㅋ.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+void input()
+{
+	cin >> n;
+}
+
+void solution()
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << "@@@@@";
+
+		cout << '\n';
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << '@';
+
+		cout << '\n';
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << "@@@@@";
+
+		cout << '\n';
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << '@';
+
+		cout << '\n';
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+			cout << '@';
+
+		cout << '\n';
+	}
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+	return 0;
+}
