64662731305fc47ea718323310f4e5cdd5e83184
Chan531 <chan657312@gmail.com>
2022-09-13 17:39:44 +0900

    백준 16928번 뱀과 사다리 게임

--- 뱀과사다리게임.cpp
+++ 뱀과사다리게임.cpp
@@ -70,3 +70,82 @@ int main(void)
 	solve();
 	return 0;
 }
+
+// 2022.09.13 12m
+/*
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int n, m, loop[101], cnt[101];
+
+void input()
+{
+	cin >> n >> m;
+
+	for (int i = 0; i < n + m; i++)
+	{
+		int a, b;
+
+		cin >> a >> b;
+
+		loop[a] = b;
+	}
+}
+
+void solution()
+{
+	for (int i = 1; i <= 100; i++)
+		cnt[i] = 1e9;
+
+	queue<int> q;
+	q.push(1);
+	cnt[1] = 0;
+
+	while (!q.empty())
+	{
+		int pos = q.front();
+		q.pop();
+
+		if (pos == 100)
+			break;
+
+		for (int i = 1; i <= 6; i++)
+		{
+			int npos = pos + i;
+
+			if (npos > 100)
+				continue;
+
+			// 뱀 또는 사다리 칸이라면 npos를 변경
+			if (loop[npos] != 0)
+				npos = loop[npos];
+
+			if (cnt[npos] > cnt[pos] + 1)
+			{
+				cnt[npos] = cnt[pos] + 1;
+				q.push(npos);
+			}
+		}
+	}
+
+	cout << cnt[100];
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+	return 0;
+}
+*/
