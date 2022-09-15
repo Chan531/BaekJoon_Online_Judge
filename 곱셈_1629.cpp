ca0c21f2338cf5e19b346389d85497c1cccbc492
Chan531 <chan657312@gmail.com>
2022-09-15 15:20:40 +0900

    백준 1629번 곱셈

--- 곱셈_1629.cpp
+++ 곱셈_1629.cpp
@@ -23,3 +23,50 @@ int main(void)
     cout << mul(a % c, b, c);
     return 0;
 }
+
+// 2022.09.15 Ç®ÀÌ
+/*
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+void input()
+{
+	cin >> a >> b >> c;
+}
+
+long long mul(int n)
+{
+	if (n == 1)
+		return a % c;
+
+	if (n % 2)
+		return mul(n / 2) * mul(n / 2 + 1) % c;
+
+	else
+		return mul(n / 2) * mul(n / 2) % c;
+}
+
+void solution()
+{
+	cout << mul(b);
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+	return 0;
+}
+*/
