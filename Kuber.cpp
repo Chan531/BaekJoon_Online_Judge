54e524af8fd2b4e2d8a435d8d23cbca9ea092f42
Chan531 <chan657312@gmail.com>
2022-11-05 23:11:00 +0900

    백준 20833번 Kuber

---
+++ Kuber.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int n, ans;
+
+void input()
+{
+	cin >> n;
+}
+
+void solution()
+{
+	for (int i = 1; i <= n; i++)
+		ans += i * i * i;
+
+	cout << ans;
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+	return 0;
+}
