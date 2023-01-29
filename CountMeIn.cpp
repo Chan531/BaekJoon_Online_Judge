6a1d4ceacdf7bd3b753f0138c3cd77a7640f704d
Chan531 <chan657312@gmail.com>
2023-01-29 14:32:01 +0900

    11319번 Count Me In

---
+++ CountMeIn.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s;
+char c[5] = { 'a', 'e', 'i', 'o', 'u' };
+
+void input()
+{
+	getline(cin, s);
+}
+
+void solution()
+{
+	int a = 0, b = 0;
+
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (!isalpha(s[i]))
+			continue;
+		
+		for (int j = 0; j < 5; j++)
+		{
+			if (c[j] == tolower(s[i]))
+			{
+				b++;
+				break;
+			}
+
+			if (j == 4)
+				a++;
+		}
+	}
+
+	cout << a << ' ' << b << '\n';
+}
+
+void solve()
+{
+	int testcase;
+	cin >> testcase;
+	cin.ignore();
+
+	while (testcase--)
+	{
+		input();
+		solution();
+	}
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+	return 0;
+}
