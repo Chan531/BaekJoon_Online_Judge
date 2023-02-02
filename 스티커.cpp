8450dbe315de41862d0df65b41ce02025f4a0112
Chan531 <chan657312@gmail.com>
2023-02-02 15:41:12 +0900

    백준 9465번 스티커

--- 스티커.cpp
+++ 스티커.cpp
@@ -1,33 +1,54 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
+int n;
+int dp[2][100000];
+
+void input()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> dp[0][i];
+
+	for (int i = 0; i < n; i++)
+		cin >> dp[1][i];
+}
+
+void solution()
+{
+	dp[0][1] += dp[1][0];
+	dp[1][1] += dp[0][0];
+
+	for (int i = 2; i < n; i++)
+	{
+		dp[0][i] += max(dp[1][i - 1], dp[1][i - 2]);
+		dp[1][i] += max(dp[0][i - 1], dp[0][i - 2]);
+	}
+
+	cout << max(dp[0][n - 1], dp[1][n - 1]) << '\n';
+}
+
+void solve()
+{
+	int testcase;
+	cin >> testcase;
+
+	while (testcase--)
+	{
+		input();
+		solution();
+	}
+}
+
 int main(void)
 {
-	int t, n, num;
-	cin >> t;
-	for (int i = 0; i < t; i++)
-	{
-		cin >> n;
-		vector<int> v, w;
-		for (int j = 0; j < 2 * n; j++)
-		{
-			cin >> num;
-			if (j < n)
-				v.push_back(num);
-			else
-				w.push_back(num);
-		}
-		v[1] += w[0];
-		w[1] += v[0];
-		for (int i = 2; i < n; i++)
-		{
-			v[i] += max(w[i - 1], w[i - 2]);
-			w[i] += max(v[i - 1], v[i - 2]);
-		}
-		cout << max(w[n - 1], v[n - 1]) << '\n';
-	}
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
 	return 0;
 }
