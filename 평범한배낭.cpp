b8d389920bbf86e3df385d888730960932f9b066
Chan531 <chan657312@gmail.com>
2023-02-02 17:19:24 +0900

    백준 12865번 평범한 배낭

--- 평범한배낭.cpp
+++ 평범한배낭.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#define p pair<int, int>
 
 using namespace std;
+
+int n, k, dp[101][100001];
+p arr[101];
+
+void input()
+{
+	cin >> n >> k;
+
+	for (int i = 1; i <= n; i++)
+		cin >> arr[i].first >> arr[i].second;
+}
+
+void solution()
+{
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= k; j++)
+		{
+			dp[i][j] = dp[i - 1][j];
+
+			if (j >= arr[i].first)
+				dp[i][j] = max(dp[i][j], dp[i - 1][j - arr[i].first] + arr[i].second);
+		}
+	}
+
+	cout << dp[n][k];
+}
+
+void solve()
+{
+	input();
+	solution();
+}
 
 int main(void)
 {
@@ -10,25 +43,6 @@ int main(void)
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int n, k, w, v;
-	cin >> n >> k;
-	vector<pair<int, int>> item(n + 1);
-	vector<vector<int>> dp(n + 1, vector<int>(k + 1));
-	for (int i = 1; i <= n; i++)
-	{
-		cin >> w >> v;
-		item[i] = { w, v };
-	}
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= k; j++)
-		{
-			if (j - item[i].first >= 0)
-				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - item[i].first] + item[i].second);
-			else
-				dp[i][j] = dp[i - 1][j];
-		}
-	}
-	cout << dp[n][k];
+	solve();
 	return 0;
 }
