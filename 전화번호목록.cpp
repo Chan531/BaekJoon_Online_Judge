06321321ca21bf4d34840d845c18415a1f8259a3
Chan531 <chan657312@gmail.com>
2023-02-16 21:32:12 +0900

    백준 5052번 전화번호 목록

--- 전화번호목록.cpp
+++ 전화번호목록.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <algorithm>
 
 using namespace std;
+
+int n;
+string num[10000];
+
+void input()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> num[i];
+}
+
+void solution()
+{
+	string answer = "YES\n";
+	sort(num, num + n);
+
+	for (int i = 1; i < n; i++)
+	{
+		int a = num[i - 1].length();
+
+		if (num[i - 1] == num[i].substr(0, a))
+		{
+			answer = "NO\n";
+			break;
+		}
+	}
+
+	cout << answer;
+}
+
+void solve()
+{
+	int testcase;
+	cin >> testcase;
+
+	while (testcase--)
+	{
+		input();
+		solution();
+	}
+}
 
 int main(void)
 {
@@ -11,42 +52,6 @@ int main(void)
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	int t, n, l1, l2;
-	string s;
-	cin >> t;
-	for (int i = 0; i < t; i++)
-	{
-		vector<string> v;
-		cin >> n;
-		for (int j = 0; j < n; j++)
-		{
-			cin >> s;
-			v.push_back(s);
-		}
-		sort(v.begin(), v.end());
-		for (int j = 0; j < n - 1; j++)
-		{
-			l1 = v[j].length();
-			l2 = v[j + 1].length();
-			if (l1 < l2)
-			{
-				if (v[j] == v[j + 1].substr(0, l1))
-				{
-					cout << "NO\n";
-					break;
-				}
-			}
-			else
-			{
-				if (v[j].substr(0, l2) == v[j + 1])
-				{
-					cout << "NO\n";
-					break;
-				}
-			}
-			if (j == n - 2)
-				cout << "YES\n";
-		}
-	}
+	solve();
 	return 0;
 }
