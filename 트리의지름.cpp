3cd4ed7945479b9b3aab4df94346345fbdf443a9
Chan531 <chan657312@gmail.com>
2023-02-20 16:15:39 +0900

    백준 1967번 트리의 지름

--- 트리의지름.cpp
+++ 트리의지름.cpp
@@ -1,57 +1,99 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstring>
+#define p pair<int, int>
+#define N 10001
 
 using namespace std;
 
-vector<vector<pair<int, int>>> v(10001);
-vector<int> visited(10001);
-queue<pair<int, int>> q;
+struct node {
+	int start, pos, dis;
+};
+
+int n, answer;
+vector<int> arr;
+vector<p> path[N];
+bool check[N], visited[N];
+
+void input()
+{
+	cin >> n;
+
+	for (int i = 1; i < n; i++)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+
+		path[a].push_back({ b, c });
+		path[b].push_back({ a, c });
+		check[a] = 1;
+	}
+}
+
+// 자식이 없는 노드 골라주기
+void setarr()
+{
+	for (int i = 1; i <= n; i++)
+		if (!check[i])
+			arr.push_back(i);
+}
+
+// 중복 체크 방지를 위해 지금까지 찾아온 자식 노드 방문 체크
+void setvisited(int k)
+{
+	for (int i = 0; i <= k; i++)
+		visited[arr[i]] = 1;
+}
+
+void solution()
+{
+	setarr();
+
+	for (int i = 0; i < arr.size(); i++)
+	{
+		memset(visited, 0, sizeof(visited));
+		queue<node> q;
+		q.push({ arr[i], arr[i], 0 });
+		setvisited(i);
+
+		while (!q.empty())
+		{
+			int start = q.front().start;
+			int pos = q.front().pos;
+			int dis = q.front().dis;
+			q.pop();
+
+			answer = max(answer, dis);
+
+			for (int j = 0; j < path[pos].size(); j++)
+			{
+				int npos = path[pos][j].first;
+
+				if (visited[npos])
+					continue;
+
+				visited[npos] = 1;
+				q.push({ start, npos, dis + path[pos][j].second });
+			}
+		}
+	}
+
+	cout << answer;
+}
+
+void solve()
+{
+	input();
+	solution();
+}
 
 int main(void)
 {
-	int n, a, b, c, max = 0;
-	cin >> n;
-	for (int i = 0; i < n - 1; i++)
-	{
-		cin >> a >> b >> c;
-		v[a].push_back(make_pair(c, b));
-		v[b].push_back(make_pair(c, a));
-	}
-	for (int i = n; i > 1; i--)
-	{
-		visited[i] = 1;
-		if (v[i][0].second > i)
-			break;
-		q.push(make_pair(v[i][0].first, v[i][0].second));
-		while (!q.empty())
-		{
-			int dis = q.front().first;
-			int node = q.front().second;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-			q.pop();
-
-			visited[node] = 1;
-
-			if (v[node].size() == 1 && visited[v[node][0].second])
-			{
-				if (dis > max)
-					max = dis;
-				continue;
-			}
-
-			for (int j = 0; j < v[node].size(); j++)
-			{
-				int ndis = v[node][j].first + dis;
-				int nnode = v[node][j].second;
-
-				if (!visited[nnode])
-					q.push(make_pair(ndis, nnode));
-			}
-		}
-		for (int j = 1; j < i; j++)
-			visited[j] = 0;
-	}
-	cout << max;
+	solve();
 	return 0;
 }
