ea44e59b4d13e8b1661297f3ac3162c2ea0ccec1
Chan531 <chan657312@gmail.com>
2023-03-18 22:05:06 +0900

    백준 20353번 Atrium

---
+++ Atrium.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double n;
+
+void input()
+{
+	cin >> n;
+}
+
+void solution()
+{
+	cout << fixed;
+	cout.precision(8);
+	cout << sqrt(n) * 4;
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+	return 0;
+}
