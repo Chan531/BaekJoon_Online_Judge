8414ed203c628960072e604202d4e947a504af78
Chan531 <chan657312@gmail.com>
2023-04-29 16:00:52 +0900

    백준 26307번 Correct

---
+++ Correct.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int h, m;
+
+void input()
+{
+	cin >> h >> m;
+}
+
+void solution()
+{
+	cout << (h - 9) * 60 + m;
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+	return 0;
+}
