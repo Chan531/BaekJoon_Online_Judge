4fcf6580b16985de6d1e3dbf3c1086a160ed1a42
Chan531 <chan657312@gmail.com>
2023-07-05 16:46:29 +0900

    백준 10815번 숫자 카드

--- 숫자카드.cpp
+++ 숫자카드.cpp
@@ -1,42 +1,49 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
-vector<int> card;
 
-int bs(int n, int left, int right)
+int n, m, arr[20000001];
+
+void input()
 {
-	int mid = (left + right) / 2;
-	if (card[mid] == n)
-		return 1;
-	if (left >= right)
-		return 0;
-	if (card[mid] > n)
-		return bs(n, left, mid - 1);
-	if (card[mid] < n)
-		return bs(n, mid + 1, right);
+	cin >> n;
+
+	while (n--)
+	{
+		int num;
+		cin >> num;
+		arr[num + 10000000] = 1;
+	}
+
+	cin >> m;
+}
+
+void solution()
+{
+	while (m--)
+	{
+		int num;
+		cin >> num;
+
+		if (arr[num + 10000000])
+			cout << 1 << ' ';
+
+		else
+			cout << 0 << ' ';
+	}
+}
+
+void solve()
+{
+	input();
+	solution();
 }
 
 int main(void)
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	cout.tie(NULL);
 
-	int n, m, num;
-	cin >> n;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> num;
-		card.push_back(num);
-	}
-	sort(card.begin(), card.end());
-	cin >> m;
-	for (int i = 0; i < m; i++)
-	{
-		cin >> num;
-		cout << bs(num, 0, n) << ' ';
-	}
+	solve();
 	return 0;
 }
