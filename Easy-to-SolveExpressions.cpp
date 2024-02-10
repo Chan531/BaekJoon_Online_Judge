ff2588c28d9307cfdb26dc7fea7219f07a0faaf2
Chan531 <chan657312@gmail.com>
2024-02-10 21:58:29 +0900

    백준 25784번 Easy-to-Solve Expressions

---
+++ Easy-to-SolveExpressions.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[3], ans = 3;
+
+void input()
+{
+	for (int i = 0; i < 3; i++) cin >> arr[i];
+}
+
+void solution()
+{
+	sort(arr, arr + 3);
+
+	if (arr[0] + arr[1] == arr[2])
+		ans = 1;
+
+	else if (arr[0] * arr[1] == arr[2])
+		ans = 2;
+
+	cout << ans;
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+	return 0;
+}
