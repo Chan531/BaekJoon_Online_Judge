7dd392ab29862018a7b4dea81ee4081444866e29
Chan531 <chan657312@gmail.com>
2024-08-04 17:41:39 +0900

    백준 11582번 치킨 TOP N

---
+++ 치킨TOPN.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n, k, arr[1 << 21];
+vector<int> v;
+
+void input()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	cin >> k;
+}
+
+void solution()
+{
+	int idx = 0;
+
+	for (int i = 0; i < k; i++)
+	{
+		for (int j = 0; j < n / k; j++)
+			v.push_back(arr[idx++]);
+
+		sort(v.begin(), v.end());
+
+		for (auto i : v)
+			cout << i << ' ';
+
+		v.clear();
+	}
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+	return 0;
+}
