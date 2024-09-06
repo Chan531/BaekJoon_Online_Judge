7e0f96b7f3a43b5c3944163d30b6856ba34d3627
Chan531 <chan657312@gmail.com>
2024-09-06 17:04:24 +0900

    백준 26742번 Skarpetki

---
+++ Skarpetki.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int b, c;
+string s;
+
+void input()
+{
+	cin >> s;
+}
+
+void solution()
+{
+	for (int i = 0; i < s.length(); i++)
+	{
+		if (s[i] == 'B') b++;
+		else c++;
+	}
+
+	cout << b / 2 + c / 2;
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+	return 0;
+}
