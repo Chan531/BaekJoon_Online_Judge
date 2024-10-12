53ecf6291c951f7bcf73f57b84c03a3c34334019
Chan531 <chan657312@gmail.com>
2024-10-12 20:30:39 +0900

    백준 27465번 소수가 아닌 수

---
+++ 소수가아닌수.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+void input()
+{
+	cin >> n;
+}
+
+void solution()
+{
+	cout << 1000000000;
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+	return 0;
+}
