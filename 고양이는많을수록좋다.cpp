d299e00dd250fe923eb5c8ce8e535da205859a26
Chan531 <chan657312@gmail.com>
2025-03-17 17:38:03 +0900

    백준 27961번 고양이는 많을수록 좋다

---
+++ 고양이는많을수록좋다.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+long long n, cur;
+int ans;
+
+void input()
+{
+	cin >> n;
+}
+
+void solution()
+{
+	while (true) 
+	{
+		if (cur >= n) break;
+
+		if (cur == 0)
+		{
+			cur = 1;
+			ans++;
+			continue;
+		}
+
+		cur *= 2;
+		ans++;
+	}
+	cout << ans;
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+	return 0;
+}
