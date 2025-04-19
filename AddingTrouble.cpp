d5bac79e696390af36a74d4a5df450d695de1787
Chan531 <chan657312@gmail.com>
2025-04-19 16:05:17 +0900

    백준 31654번 Adding Trouble

---
+++ AddingTrouble.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int a, b, c;
+
+int main(void) {
+    cin >> a >> b >> c;
+    if (a + b == c) cout << "correct!";
+    else cout << "wrong!";
+    return 0;
+}
