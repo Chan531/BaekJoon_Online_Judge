b145bf3465aeebe70ee4b7e95e2ab8dfe22c14f3
Chan531 <chan657312@gmail.com>
2025-04-26 17:24:16 +0900

    백준 17216번 가장 큰 감소 부분 수열

---
+++ 가장큰감소부분수열.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, arr[1010], dp[1010];
+
+void input()
+{
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+}
+
+void solution()
+{
+	for (int i = 0; i < n; i++)
+	{
+		dp[i] = arr[i];
+
+		for (int j = 0; j < i; j++)
+		{
+			if (arr[i] < arr[j])
+			{
+				dp[i] = max(dp[i], dp[j] + arr[i]);
+			}
+		}
+	}
+
+	cout << *max_element(dp, dp + n);
+}
+
+void solve()
+{
+	input();
+	solution();
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	solve();
+	return 0;
+}
